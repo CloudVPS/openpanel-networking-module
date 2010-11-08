ca22fcf606f07eeb8b83373c4a87f93c3f3e69f6
Koert van der Veer <koert@openpanel.com>
2010-11-08 15:21:08 +0100

    Applied GPL&LGPL where applicable. refs #606

--- addflavor.sh
+++ addflavor.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+# This file is part of OpenPanel - The Open Source Control Panel
+# OpenPanel is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free 
+# Software Foundation, using version 3 of the License.
+#
+# Please note that use of the OpenPanel trademark may be subject to additional 
+# restrictions. For more information, please visit the Legal Information 
+# section of the OpenPanel website on http://www.openpanel.com/
+
+
 if grep ^CXXFLAGS makeinclude | grep -q LINUX_REDHAT ; then
  cp rsrc/openpanel.module.networking.conf.xml.redhat rsrc/openpanel.module.networking.conf.xml
 elif grep ^CXXFLAGS makeinclude | grep -q LINUX_DEBIAN ; then
